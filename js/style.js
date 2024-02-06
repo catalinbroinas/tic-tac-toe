b46baafd4c4470334b5ca82f19a0be341132d73c
catalinbroinas <catalinbroinas@yahoo.com>
2024-02-06 19:48:52 +0200

    Add game board display and player initialization

--- js/style.js
+++ js/style.js
@@ -6,7 +6,7 @@ function Gameboard() {
     for (let i = 0; i < rows; i++) {
         gameboard[i] = [];
         for (let j = 0; j < cols; j++) {
-            gameboard[i][j] = j;
+            gameboard[i][j] = null;
         }
     }
 
@@ -17,8 +17,18 @@ function Gameboard() {
 
 function Players() {
     const gameboard = Gameboard();
-    const playerOne = {};
-    const playerTwo = {};
+    const playerOne = {
+        marker: 'X',
+        name: 'Player One',
+        flowControl: true
+    };
+    const playerTwo = {
+        marker: '0',
+        name: 'Player Two',
+        flowControl: false
+    };
+
+    return { playerOne, playerTwo };
 }
 
 function Cell() {
@@ -28,3 +38,20 @@ function Cell() {
 function Game() {
     const gameboard = Gameboard();
 }
+// Display array for test 
+function displayGameboard() {
+    const gameboard = Gameboard();
+    value = 0;
+
+    gameboard.getGameboard().forEach((rows) => {
+        rows.forEach((cols) => {
+            cols = value;
+            console.log(value);
+            value++;
+        });
+    });
+
+    console.table(gameboard.getGameboard());
+}
+
+displayGameboard();
